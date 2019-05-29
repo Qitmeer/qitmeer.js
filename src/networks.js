04924df7e9d358d6b37a77f491ba18c7aaaa6332
zhangyi999 <zhang_btc@outlook.com>
2019-05-29 14:11:00 +0800

    add btc atomicswap

--- src/networks.js
+++ src/networks.js
@@ -2,14 +2,17 @@
 module.exports = {
   mainnet: {
     pubKeyHashAddrId: 0x0c41,
-    ScriptHashAddrID: 0x0c12
+    ScriptHashAddrID: 0x0c12,
+    PrivateKeyID: 0x0cd1
   },
   testnet: {
     pubKeyHashAddrId: 0x0f12,
-    ScriptHashAddrID: 0x0ee2
+    ScriptHashAddrID: 0x0ee2,
+    PrivateKeyID: 0x0ce2
   },
   privnet: {
     pubKeyHashAddrId: 0x0df1,
-    ScriptHashAddrID: 0x0dc2
+    ScriptHashAddrID: 0x0dc2,
+    PrivateKeyID: 0x0cdd
   }
 }
