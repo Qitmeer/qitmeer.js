dda2004b7e0a1a748e228a9d1155d6262719416b
frankcrypto <frankcrypto@gmail.com>
2024-08-03 17:34:58 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
-const _OPS = require("./ops/ops.json");
-import types from "./types";
-import typecheck from "./typecheck";
-import * as hash from "./hash";
-import * as ec from "./ec";
-import qitmeer58check from "./qitmeer58check";
-import * as address from "./address";
-import { networks } from "./networks";
-import Transaction from "./transaction";
-import txsign from "./txsign";
-import block from "./block";
-import OPS_MAP from "./ops/map";
-import script from "./script";
-import * as signature from "./signature";
+import types from './types'
+import typecheck from './typecheck'
+import * as hash from './hash'
+import * as ec from './ec'
+import qitmeer58check from './qitmeer58check'
+import * as address from './address'
+import { networks } from './networks'
+import Transaction from './transaction'
+import txsign from './txsign'
+import block from './block'
+import OPS_MAP from './ops/map'
+import script from './script'
+import * as signature from './signature'
+const _OPS = require('./ops/ops.json')
 
 export {
   types,
@@ -31,5 +31,5 @@ export {
   _OPS as OPS,
   OPS_MAP,
   script,
-  signature,
-};
+  signature
+}
