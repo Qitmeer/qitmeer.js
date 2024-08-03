891b2427da0fe58e3b81e99c1135df83558dd116
frankcrypto <frankcrypto@gmail.com>
2024-08-03 17:04:57 +0800

    fix test

--- test/test.ts
+++
@@ -1,7 +0,0 @@
-import { suite, it } from "mocha";
-suite("Calculator Tests", () => {
-  it("should return 5 when 2 is added to 3", () => {
-    const result = 2 + 3;
-    console.log(result);
-  });
-});
